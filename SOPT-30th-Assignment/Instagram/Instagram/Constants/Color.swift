e243a831929521a4643651bdf5d89eb6a3fabc23
Sejin Lee <lsj8706@naver.com>
2022-04-12 21:22:54 +0900

    [ADD] : Assets 추가

---
+++ SOPT-30th-Assignment/Instagram/Instagram/Constants/Color.swift
@@ -0,0 +1,17 @@
+//
+//  Color.swift
+//  Instagram
+//
+//  Created by User on 2022/04/12.
+//
+
+import UIKit
+
+extension Const {
+    
+    struct Color {
+        // Auth 버튼 색상
+        static let button_blocked = UIColor(named: "button_blocked")
+    }
+    
+}
--- SOPT-30th-Assignment/Instagram/Instagram/Constants/Image.swift
+++ SOPT-30th-Assignment/Instagram/Instagram/Constants/Image.swift
@@ -11,8 +11,22 @@ extension Const {
         
     struct Image {
         // 텍스트 필드 아이콘
+        static let clear_circle = UIImage(systemName: "xmark.circle")
         static let password_shown = UIImage(named: "password_shown_eye_icon")
         static let password_hidden = UIImage(named: "password_hidden_eye_icon")
+        
+        // 탭바 아이콘
+        static let home = UIImage(named: "icn_home")
+        static let home_selected = UIImage(named: "icn_home_selected")
+        static let profile = UIImage(named: "icn_profile")
+        static let profile_selected = UIImage(named: "icn_profile_selected")
+        static let reels = UIImage(named: "icn_reels")
+        static let reels_selected = UIImage(named: "icn_reels_selected")
+        static let search = UIImage(named: "icn_search")
+        static let search_selected = UIImage(named: "icn_selected")
+        static let shop = UIImage(named: "icn_shop")
+        static let shop_selected = UIImage(named: "icn_shop_selected")
+
     }
     
 }
