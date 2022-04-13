b277b700af1b7f9c33ab458199621fa3f61c252b
Sejin Lee <lsj8706@naver.com>
2022-04-14 02:46:40 +0900

    [FEAT] : 릴스 버튼 클릭시 탭 바 속성 변경

--- SOPT-30th-Assignment/Instagram/Instagram/Controllers/TabBarControllers/TabBarController.swift
+++ SOPT-30th-Assignment/Instagram/Instagram/Controllers/TabBarControllers/TabBarController.swift
@@ -9,8 +9,45 @@ import UIKit
 
 class TabBarController: UITabBarController {
     
+    //MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setUI()
+        configureTabBar()
     }
     
+    //MARK: - Helpers
+    func setUI() {
+        tabBar.unselectedItemTintColor = .black
+    }
+    
+    func configureTabBar() {
+        self.delegate = self
+    }
+    
+    func changeTabBarColor(toBlack: Bool){
+        if toBlack {
+            tabBar.backgroundColor = .black
+            tabBar.tintColor = .white
+            tabBar.unselectedItemTintColor = .white
+        } else {
+            tabBar.backgroundColor = .white
+            tabBar.tintColor = .black
+            tabBar.unselectedItemTintColor = .black
+        }
+    }
+}
+
+//MARK: - UITabBarControllerDelegate
+extension TabBarController: UITabBarControllerDelegate {
+    
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        
+        let index = viewControllers?.firstIndex(of: viewController)
+        
+        (index == 2) ? changeTabBarColor(toBlack: true) : changeTabBarColor(toBlack: false)
+        
+        return true
+    }
 }
