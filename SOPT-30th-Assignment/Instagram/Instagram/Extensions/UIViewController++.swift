519e12d5e1fdad05c20f9c4bc6facf4d881647ba
Sejin Lee <lsj8706@naver.com>
2022-05-17 01:51:45 +0900

    [CHORE] : alert 함수 escaping 제거

--- SOPT-30th-Assignment/Instagram/Instagram/Extensions/UIViewController++.swift
+++ SOPT-30th-Assignment/Instagram/Instagram/Extensions/UIViewController++.swift
@@ -13,15 +13,9 @@ extension UIViewController {
         self.navigationController?.navigationBar.isHidden = isHidden
     }
 
-    func alert(withTitle title: String, message: String, completion: @escaping(UIAlertAction) -> Void) {
+    func alert(withTitle title: String, message: String, completion: ((UIAlertAction) -> Void)? = nil) {
         let alertVC = UIAlertController(title: title, message: message, preferredStyle: .alert)
         alertVC.addAction(UIAlertAction(title: "확인", style: .default, handler: completion))
-        present(alertVC, animated: true)
-    }
-    
-    func alert(withTitle title: String, message: String) {
-        let alertVC = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        alertVC.addAction(UIAlertAction(title: "확인", style: .default, handler: nil))
         present(alertVC, animated: true)
     }
 }
